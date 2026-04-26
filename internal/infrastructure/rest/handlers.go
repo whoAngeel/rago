d2ce0a444d194938839a07ca9a7c785bb9e32357
azorrilla-alt <whoangel.agl@gmail.com>
2026-04-26 17:30:28 -0600

    creating http server with gin

---
+++ internal/infrastructure/rest/handlers.go
@@ -0,0 +1,39 @@
+package rest
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/whoAngeel/rago/internal/core/ports"
+)
+
+func NewRouter(logger ports.Logger) http.Handler {
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(requestLogger(logger))
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	return r
+}
+
+func requestLogger(logger ports.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+
+		c.Next()
+
+		logger.Info(
+			"incoming request",
+			"method", c.Request.Method,
+			"path", path,
+			"latency", time.Since(start),
+		)
+	}
+}
---
+++ internal/infrastructure/rest/server.go
@@ -0,0 +1,39 @@
+package rest
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/whoAngeel/rago/internal/core/ports"
+)
+
+type Server struct {
+	server *http.Server
+	logger ports.Logger
+}
+
+func NewServer(host, port string, router http.Handler, logger ports.Logger) *Server {
+
+	return &Server{
+		server: &http.Server{
+			Addr:         fmt.Sprintf("%s:%s", host, port),
+			Handler:      router,
+			ReadTimeout:  60 * time.Second,
+			WriteTimeout: 60 * time.Second,
+			IdleTimeout:  120 * time.Second,
+		},
+		logger: logger,
+	}
+}
+
+func (s *Server) Start() error {
+	s.logger.Info("server starting at", "addr", s.server.Addr)
+	return s.server.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	s.logger.Info("shutting down server")
+	return s.server.Shutdown(ctx)
+}
