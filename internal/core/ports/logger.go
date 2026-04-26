e54b33ca6240644502c360eb56028d647ca59e1d
azorrilla-alt <whoangel.agl@gmail.com>
2026-04-26 17:30:36 -0600

    logger port

---
+++ internal/core/ports/logger.go
@@ -0,0 +1,11 @@
+package ports
+
+type Logger interface {
+	Debug(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
+	Error(msg string, args ...any)
+	Fatal(msg string, args ...any)
+
+	With(args ...any) Logger
+}
