a08e183ecd08c98f26ef53df5cddfb9e9eb9380b
azorrilla-alt <whoangel.agl@gmail.com>
2026-04-27 00:39:52 -0600

    initialize services

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,8 +9,11 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/whoAngeel/rago/internal/application"
 	"github.com/whoAngeel/rago/internal/infrastructure/config"
 	"github.com/whoAngeel/rago/internal/infrastructure/logger"
+	"github.com/whoAngeel/rago/internal/infrastructure/openrouter"
+	"github.com/whoAngeel/rago/internal/infrastructure/qdrant"
 	"github.com/whoAngeel/rago/internal/infrastructure/rest"
 )
 
@@ -18,12 +21,38 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
-	cfg, _ := config.Load()
+	cfg, err := config.Load()
+	if err != nil {
+		panic("failed to load config: " + err.Error())
+	}
 	log := logger.New(cfg.Env)
 
 	log.Info("App starting", "mode", cfg.Env, "port", cfg.Port)
 
-	router := rest.NewRouter(log)
+	// Inicializar servicios
+	vStore, err := qdrant.NewQdrantAdapter(cfg.QdrantHost, cfg.QdrantPort)
+	if err != nil {
+		log.Fatal("error initializing qdrant", "error", err)
+	}
+
+	llm, err := openrouter.NewOpenRouterAdapter(
+		cfg.OpenRouterKey, cfg.OpenRouterBaseUrl, cfg.Model,
+	)
+	if err != nil {
+		log.Fatal("error initializing llm", "error", err)
+	}
+
+	embedder, err := openrouter.NewEmbedderAdapter(cfg.OpenRouterKey, cfg.OpenRouterBaseUrl, cfg.EmbeddingModel)
+	if err != nil {
+		log.Fatal("error initializing embedder", "error", err)
+	}
+
+	router := rest.NewRouter(log, &rest.Handlers{
+		AskHandler: rest.NewAskHandler(
+			application.NewAskUsecase(vStore, llm, log, embedder, cfg),
+			log,
+		),
+	})
 	server := rest.NewServer(cfg.Host, cfg.Port, router, log)
 
 	serverErr := make(chan error, 1)
