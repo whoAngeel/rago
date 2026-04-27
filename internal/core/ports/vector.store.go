cb648995d1eeba56259e62bc6e9d3d5dbb9c1eb6
azorrilla-alt <whoangel.agl@gmail.com>
2026-04-26 23:26:27 -0600

    fix typos on interface

--- internal/core/ports/vector.store.go
+++ internal/core/ports/vector.store.go
@@ -9,12 +9,12 @@ import (
 type VectorStore interface {
 	CreateCollection(ctx context.Context, name string, size int) error
 	UpsertDocuments(ctx context.Context, collection string, docs []schema.Document, vectors [][]float32) error
-	Search(ctx context.Context, queryVector []float32, limit int) (SearchResult, error)
-	GetPointsCount(ctx context.Context, collection string) (int64, error)
-	DeleteColletion(ctx context.Context, collection string) error
+	Search(ctx context.Context, collection string, queryVector []float32, limit int) ([]SearchResult, error)
+	GetPointsCount(ctx context.Context, collection string) (uint64, error)
+	DeleteCollection(ctx context.Context, collection string) error
 }
 
 type SearchResult struct {
 	Document schema.Document
-	score    float32
+	Score    float32
 }
