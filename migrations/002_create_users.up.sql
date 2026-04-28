327d86004b1a64afd9f6967a9cd730b00e736b97
azorrilla-alt <whoangel.agl@gmail.com>
2026-04-27 23:01:42 -0600

    deleting

--- migrations/002_create_users.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    role_id INTEGER REFERENCES roles(id),
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-);
