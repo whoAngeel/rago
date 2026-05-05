999dff6e67fd14ac7c060444af94744c4dba3941
azorrilla-alt <whoangel.agl@gmail.com>
2026-05-05 00:16:39 -0600

    chore config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tanstackRouter({
+    target: 'react',
+    autoCodeSplitting: true
+  })],
 })
